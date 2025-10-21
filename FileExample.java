9299b3b2d204c07155578e9701bd27538ff68b50
Antonio-Vo <34730@ocvts.org>
2025-10-21 13:00:01 +0000

    added to private static

---
+++ FileExample.java
@@ -0,0 +1,63 @@
+/*
+ * NAME: 
+ * DATE: 
+ * VERSION: 
+ * SOURCES USED: 
+ * COMMENTS: 
+ * 
+ * REQUIREMENTS: 
+*/
+
+// IMPORT STREAMS // 
+import java.io.*; 
+import java.util.*; 
+
+public class FileExample {
+    
+    // File Name
+    private static final String FILE_NAME = "task.txt";
+    // ArrayList 
+    private static ArrayList<String> tasks = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // Load Task 
+        Scanner sc = new Scanner(System.in); 
+        boolean running = true; 
+
+        while (running){
+            // MENU 
+            // ---> Menu List 
+            // ---> User Input
+            // IF/ELSE or SWITCH for the menu
+        }
+
+        // Save Task
+        // Close Scanner
+
+    }// END OF MAIN
+
+    // - - - - - - - - - FUNCTIONS  - - - - - - - - - // 
+
+    // Function 1 - loadTask()    // To load the File
+
+    // Function 2 - saveTask()    // To save the File
+
+    // Function 3 - viewTask()    // To view the File
+
+
+    // Function 4 - addTask()     // To add information to the file
+    // Function 5 - removeTask()  // To remove information from the file
+    // Error Handling 
+    
+    // - - - - - - - - - OTHER REQUIREMENTS - - - - - - - - - // 
+    // Function 6 - check the status of the task
+    // Function 7 - check the status of the task
+    // Function 8 - check the status of the task
+
+    // - - - - - - - - - ADDITIONAL FEATURES - - - - - - - - - // 
+
+    // Function 9 - priorityTask() 
+    // Function 10 - searchTask() 
+    // Function 11 - sortTask() 
+
+}// END OF CLASS 
