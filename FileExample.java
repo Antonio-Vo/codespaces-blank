65c3e5656bb5859a05a7e31c008d259bee84ca01
Antonio-Vo <34730@ocvts.org>
2025-10-21 13:18:35 +0000

    added tasks functions

--- FileExample.java
+++ FileExample.java
@@ -37,13 +37,41 @@ public class FileExample {
     }// END OF MAIN
 
     // - - - - - - - - - FUNCTIONS  - - - - - - - - - // 
+    /*
+     * .remove()
+     * .canRead()
+     * .add()
+     * .isEmpty()
+     */
 
     // Function 1 - loadTask()    // To load the File
+    private static void loadTask() throws IOException {
+        File file = new File("File_Name");\
+        if (file.exists()) {
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                tasks.add(fileReader.nextLine());
+            }
+            fileReader.close();
+        }
+    }
 
     // Function 2 - saveTask()    // To save the File
-
+    private static void saveTask() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("FILE_NAME"));
+        for (String task : tasks) {
+            write.write(task);
+            writer.newLine();
+        }
+        writer.close();
+    }
     // Function 3 - viewTask()    // To view the File
-
+    private static void viewTask() {
+        if (tasks.isEmpty()) System.out.println("No tasks");
+        else for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i+1) + ". " + tasks.get(i));
+        }
+    }
 
     // Function 4 - addTask()     // To add information to the file
     // Function 5 - removeTask()  // To remove information from the file
